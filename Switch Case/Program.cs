1b93c6711da68f391276cbbe50020f061cb2dae0
ThogoGakinya <thogosamson@gmail.com>
2024-01-16 18:49:53 +0300

    Add project files.

---
+++ Switch Case/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Switch_Case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Please enter your Age");
+            string enterAge = Console.ReadLine();
+            bool myAge = int.TryParse(enterAge, out age);
+
+            if (myAge)
+            {
+                switch (age)
+                {
+                    case 15:
+                        Console.WriteLine("Under Age");
+                        break;
+                    case 30:
+                        Console.WriteLine("Over Age");
+                        break;
+                    case 25:
+                        Console.WriteLine("Correct Age");
+                        break;
+                    default:
+                        Console.WriteLine("Not recognized");
+                        break;
+
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Wrong Age Format");
+            }
+        }
+    }
+}
